849d29e1ff29b714d755e7f1b5a9b54add9ce0c5
Mambetova Gulizar <iamgulizar@gmal.com>
2018-12-20 01:53:16 +0300

    Update naive.r

--- bayes  classifiers/naive.r
+++ bayes  classifiers/naive.r
@@ -1,23 +1,23 @@
-naiv <- function(x, mu, sigma, lambda, P){
+naive <- function(xi, mu, sigma, lambda, P){
   n <- 2
   res <- log(lambda*P)
   for(i in 1 : n){
-    pyj <- getPyj(sigma[i], x[i], mu[i])
+    pyj <- getPyj(sigma[i], xi[i], mu[i])
     res <- res + log(pyj)
   }
   return(res)
 }
 
-getPyj <- function(disp, x, mu){
-  return((1/(disp*sqrt(2*pi))) * exp(-1 * ((x - mu)^2)/(2*disp^2)) )
+getPyj <- function(disp, xi, mu){
+  return((1/(disp*sqrt(2*pi))) * exp(-1 * ((xi - mu)^2)/(2*disp^2)) )
 }
 
-get_mu_with_hat <- function(xl){
+mu_with_hat <- function(xl){
   l <- dim(xl)[1] 
   return(c(sum(xl[,1])/l, sum(xl[,2])/l))
 }
 
-get_sigma_with_hat <- function(xl, mu){
+sigma_with_hat <- function(xl, mu){
   l <- dim(xl)[1] 
   
   return(c(sum((xl[,1] - mu[1])^2)/l, sum((xl[,2] - mu[2])^2)/l))
@@ -28,7 +28,7 @@ quality <- function(xl, muh1, sigma1, muh2, sigma2){
   mis <- 0
   for(i in 1 : l){      
     class <- 0;
-    if(naiv(xl[i, -3], muh1, sigma1, 1, 0.5) > naiv(xl[i, -3], muh2, sigma2, 1, 0.5)){
+    if(naive(xl[i, -3], muh1, sigma1, 1, 0.5) > naive(xl[i, -3], muh2, sigma2, 1, 0.5)){
       class <- 1
     } else {
       class <- 2
@@ -56,8 +56,8 @@ main <- function(objectCounter = 500){
   plot(xl[,1],xl[,2], pch = 21,main = "Наивный байесовский классификатор", xlab = 'Признак 1', ylab= 'ризнак 2', col = colors[xl[,3]], asp = 1, bg=colors[xl[,3]])
   
   colors <- c("purple", "#FF3300")
-  muh1 <- get_mu_with_hat(x1)
-  muh2 <- get_mu_with_hat(x2)         
+  muh1 <- mu_with_hat(x1)
+  muh2 <- mu_with_hat(x2)         
   sigma1 <- get_sigma_with_hat(x1, muh1)
   sigma2 <- get_sigma_with_hat(x2, muh2)
   
@@ -66,12 +66,12 @@ main <- function(objectCounter = 500){
     x2 <- -8;
     while(x2 < 13){          
       class <- 0;
-      if(naiv(c(x1,x2), muh1, sigma1, 1, 0.5) > naiv(c(x1,x2), muh2, sigma2, 1, 0.5)){
+      if(naive(c(x1,x2), muh1, sigma1, 1, 0.5) > naive(c(x1,x2), muh2, sigma2, 1, 0.5)){
         class <- 1
       } else {
         class <- 2
       }
-      #points(x1, x2, pch = 21, col=colors[class], asp = 1)
+ 
       x2 <- x2 + 0.2
     }
     x1 <- x1 + 0.2
