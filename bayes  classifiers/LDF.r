98af6f5214cd57b1f92f1e576d8aac17fb17077e
icyvan <iamgulizar@gmail.com>
2019-01-24 23:52:55 +0300

    LDF

--- bayes  classifiers/LDF.r
+++ bayes  classifiers/LDF.r
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(MASS)
 estimateMu = function(points) {
   rows = dim(points)[1]
@@ -8,27 +7,6 @@ estimateMu = function(points) {
     mu[1, col] = mean(points[, col])
   }
   return(mu)
-=======
-data(iris)
-x <- cbind(iris$Petal.Length,iris$Petal.Width)
-Y <- ifelse(iris$Species == "virginica", +1, -1)
-
-u <- apply(x,2,mean)
-
-up <- apply(subset(x,Y==+1), 2, mean)
-un <- apply(subset(x,Y==-1), 2, mean)
-np <- sum(Y==+1)
-nn <- sum(Y==-1)
-
-t <- seq(- pi , pi, 0.05)
-
-uv <- cbind(cos(t), sin(t))
-
-SB <- nn * (un - u) %*% t(up - u)
-
-scatter <- function(v){
-  ((v - un) %*% t(v - un)) + ((v - up) %*% t(v - up))
->>>>>>> ccd580f0dffe6ada43efc3eef7eab97f930d50d9
 }
 estimateFisherCovarianceMatrix <- function(objects1, objects2, mu1, mu2)
 {
@@ -47,12 +25,11 @@ estimateFisherCovarianceMatrix <- function(objects1, objects2, mu1, mu2)
   }
   return (sigma)
 }
-<<<<<<< HEAD
 ## Ãåíåðèðóåì òåñòîâûå äàííûå
 Sigma1 <- matrix(c(5, 0, 0, 1), 2, 2)
 Sigma2 <- matrix(c(5, 0, 0, 1), 2, 2)
-Mu1 <- c(1, 0)
-Mu2 <- c(10, 0)
+Mu1 <- c(5, 1)
+Mu2 <- c(15, 0)
 xy1 <- mvrnorm(n=ObjectsCountOfEachClass, Mu1, Sigma1)
 xy2 <- mvrnorm(n=ObjectsCountOfEachClass, Mu2, Sigma2)
 ## Ñîáèðàåì äâà êëàññà â îäíó âûáîðêó
@@ -74,28 +51,3 @@ mu_st <- (mu1 + mu2) / 2
 beta <- mu_st %*% alpha
 ## Ðèñóåì ËÄÔ
 abline(beta / alpha[2,1], -alpha[1,1]/alpha[2,1], col = "red", lwd = 3)
-=======
-
-ratios <- apply(uv, 1, action, SB) / apply(uv, 1, action, SW)
-
-mr <- which.max(ratios)
-muv <- uv[mr,]
-mv <- 40*ratios[mr]*muv
-xp <- as.vector(x %*% muv)
-rxp <- round(range(xp),0)+c(-1,1)
-xpn <- subset(xp,Y==-1)
-xpp <- subset(xp,Y==+1)
-b = (mean(xpp) * sd(xpn) + mean(xpn) * sd(xpp)) /(sd(xpp) + sd(xpn))
-plot(x,col=Y+3,asp=1, xlab = "Sepal.Length", ylab = "Sepal.Width", main = paste("Ã‹Ã¨Ã­Ã¥Ã©Ã­Ã»Ã© Ã¤Ã¨Ã±ÃªÃ°Ã¨Ã¬Ã¨Ã­Ã Ã­Ã² Ã”Ã¨Ã¸Ã¥Ã°Ã "))
-par(lwd=2)
-abline(b/muv[2],-muv[1]/muv[2])
-distance.from.plane = function(x,w,b) {
-  b - sum(x*w)
-}
-classify.fisher = function(x,w,b) {
-  distances = apply(x, 1, distance.from.plane, w, b)
-  return(ifelse(distances < 0, -1, +1))
-}
-
-sum(abs(Y - classify.fisher(x,muv,b) ))
->>>>>>> ccd580f0dffe6ada43efc3eef7eab97f930d50d9
